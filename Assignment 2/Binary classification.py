f4b41cb96c31c7cee274bfefae2d51afe222a868
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2019-09-21 22:13:21 -0400

    update

--- Assignment 2/Binary classification.py
+++ Assignment 2/Binary classification.py
@@ -6,10 +6,10 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 from tqdm import trange
-LAMBDA = .001
+LAMBDA = .01
 NUM_SAMP = 250
-BATCH_SIZE = 50
-NUM_BATCHES = 100
+BATCH_SIZE = 32
+NUM_BATCHES = 100000
 LEARNING_RATE = 0.01
 
 class Data(object):
@@ -22,15 +22,15 @@ class Data(object):
 
         self.t = np.random.uniform(size=(num_samp))* 3.5 *np.pi 
         self.x1 = np.float32((-2-(5.5+np.random.normal(0,sigma,num_samp))*self.t) * np.cos(self.t))
-        self.y1 = np.float32((-2-(5.5+np.random.normal(0,sigma,num_samp))*self.t) * np.sin(self.t))
-
         self.x2 = np.float32((2+(5.5+np.random.normal(0,sigma,num_samp))*self.t) * np.cos(self.t) + 3)
+
+        self.y1 = np.float32((-2-(5.5+np.random.normal(0,sigma,num_samp))*self.t) * np.sin(self.t))
         self.y2 = np.float32((2+(5.5+np.random.normal(0,sigma,num_samp))*self.t) * np.sin(self.t) -3 ) 
 
-        self.setone = np.concatenate((self.x1,self.y1),axis=0)
-        self.settwo = np.concatenate((self.x2,self.y2),axis=0)
+        self.setone = np.transpose(np.vstack((self.x1,self.y1)))
+        self.settwo = np.transpose(np.vstack((self.x2,self.y2)))
 
-        self.coord= np.transpose(np.vstack((self.setone,self.settwo)))
+        self.coord= np.vstack((self.setone,self.settwo))
         self.activation = np.concatenate((np.float32(np.zeros(num_samp)),np.float32(np.ones(num_samp))), axis=0)
 
     def get_batch(self, batch_size=BATCH_SIZE):
@@ -54,26 +54,24 @@ class MLP(tf.Module):
     def __call__(self, points):
         #Neural Network
         layer1 = tf.matmul(points, self.w1) +self.b1
-        activ1 = tf.nn.relu(layer1)  
+        activ1 = tf.nn.elu(layer1)  
         layer2 = tf.matmul(activ1, self.w2) + self.b2
-        activ2 = tf.nn.relu(layer2)
+        activ2 = tf.nn.elu(layer2)
         layer3 = tf.matmul(activ2,self.w3) + self.b3
         #more layers to come
         return tf.squeeze(layer3)
 
 class Draw():
-    def __init__(self):
+    def __init__(self,xx,yy,yyy):
         fig, axarr = plt.subplots(nrows=1, ncols=1, figsize=[8,8])
 
-        #Display Boundary
-        xvalues = np.array([0, 1, 2, 3, 4])
-        yvalues = np.array([0, 1, 2, 3, 4])
-        xx, yy = np.meshgrid(xvalues, yvalues)
-        plt.plot(xx, yy, marker='.', color='k', linestyle='none')
         
+        plt.contourf(xx, yy, yyy, [0,.5,1])
+
         #Display example Data
-        plt.scatter(data.x1,data.y1,label='Data 1')
-        plt.scatter(data.x2,data.y2,label='Data 2')
+        plt.scatter(data.x1,data.y1,label='Data 0')
+        plt.scatter(data.x2,data.y2,label='Data 1')
+        
 
         axarr.set_title('Binary Classification')
         axarr.set(xlabel='x', ylabel='y')
@@ -81,10 +79,6 @@ class Draw():
         plt.legend()
     
         plt.show()
-
-def loss(mlp, x, y):
-  y_ = mlp(x)
-  return tf.nn.sigmoid_cross_entropy_with_logits(labels=activations, logits=y_) + (LAMBDA*tf.norm(mlp.w1)**2)
 
 if __name__ == "__main__":
     data = Data()
@@ -97,18 +91,23 @@ if __name__ == "__main__":
         
         with tf.GradientTape() as tape:
             coords, activations = data.get_batch()
-            loss = (mlp,coords,activations)
-            
+            y_hat = mlp(coords)
+            loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=activations, logits=y_hat) + (LAMBDA*tf.norm(mlp.w1)**2))
         grads = tape.gradient(loss, mlp.variables)
         optimizer.apply_gradients(zip(grads, mlp.variables))
         bar.refresh()
     
+
+    x = y = np.linspace(-60,60,300)
+    xf = np.float32(x)
+    yf = np.float32(y)
+
+    xx, yy = np.meshgrid(xf,yf)
+    allp = np.array(list(zip(xx.flatten(), yy.flatten())))
+    y_hat = mlp(allp)
     
-    """ coords_test,activation_test= data.get_batch()
-    y_hat = mlp(coords_test)
-    loss2= tf.sigmoid(y_hat) 
-    print(np.round(loss2)) """
+    yyy = tf.math.sigmoid(y_hat)
+    pleasesendhelp = np.reshape(yyy,(300,300))
 
-    draw = Draw()
-
+    draw = Draw(xx,yy,pleasesendhelp)
     
